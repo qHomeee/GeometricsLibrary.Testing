2c01b66c6e2142cf54b630f0593811a875f1b576
unknown <@ngknn.local>
2026-04-02 14:37:01 +0300

    FC

---
+++ GeometricsLibrary.Testing/Test1.cs
@@ -0,0 +1,15 @@
+﻿using Geometrics;
+namespace GeometricsLibrary.Testing
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
+
+
